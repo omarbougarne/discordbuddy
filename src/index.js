28e0135f31ca9e12d51e44c628c5d4238c63c806
omarbougarne <omarbougarne98@gmail.com>
2025-04-11 16:23:39 +0100

    Up and ready again addin script to run client once when ready

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once(Events.ClientReady, readyClient =>{
+     console.log(`Client ready ${readyClient.user.tag}`);
+     
+})
 const TOKEN = process.env.TOKEN;
 client.login(TOKEN).catch(console.error);
